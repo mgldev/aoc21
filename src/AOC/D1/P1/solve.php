a9c545ca7167121663e30ccbcdd05232566c68a7
Michael Leach <michael@mgldev.co.uk>
2021-12-08 15:40:35 +0000

    Completes D1

--- src/AOC/D1/P1/solve.php
+++ src/AOC/D1/P1/solve.php
@@ -6,4 +6,16 @@ use AOC\Helper\InputReader;
 
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 
-$numbers = InputReader::fileToLines(__DIR__ . '/../input.txt');
+$depths = InputReader::fileToLines(__DIR__ . '/../input.txt');
+
+$prevDepth = null;
+$depthsHigherThanPrevious = 0;
+
+foreach ($depths as $depth) {
+    if ($prevDepth !== null && $depth > $prevDepth) {
+        $depthsHigherThanPrevious++;
+    }
+    $prevDepth = $depth;
+}
+
+die('Part 1: ' . $depthsHigherThanPrevious . "\n");
--- src/AOC/D1/P2/solve.php
+++ src/AOC/D1/P2/solve.php
@@ -6,4 +6,15 @@ use AOC\Helper\InputReader;
 
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 
-$numbers = InputReader::fileToLines(__DIR__ . '/../P1/input.txt');
+$numbers = InputReader::fileToLines(__DIR__ . '/../input.txt');
+$count = count($numbers);
+$sum = fn (array $indexes) => array_sum(array_map(fn (int $index) => $numbers[$index], $indexes));
+$countSumsLargerThanPrev = 0;
+
+for ($i = 1; $i < $count - 2; $i++) {
+    $curIndexes = [$i, $i + 1, $i + 2];
+    $prevIndexes = array_map(fn (int $index) => $index - 1, $curIndexes);
+    $countSumsLargerThanPrev += (int) $sum($curIndexes) > $sum($prevIndexes);
+}
+
+echo 'Part 2: ' . $countSumsLargerThanPrev . "\n";
