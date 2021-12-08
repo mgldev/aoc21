419f37378108f8a3e18096b41634e1e74600de09
Michael Leach <michael@mgldev.co.uk>
2021-12-08 16:53:04 +0000

    Restores original D2P1 code

--- src/AOC/D2/P1/solve.php
+++ src/AOC/D2/P1/solve.php
@@ -8,30 +8,19 @@ require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 $lines = InputReader::fileToLines(__DIR__ . '/../input.txt');
 
-$forward = $aim = $depth = 0;
+$distances = [];
 
 foreach ($lines as $line) {
     $matches = [];
     preg_match('/^(?<direction>\w+) (?<distance>\d+)$/', $line, $matches);
-    $direction = $matches['direction'];
-    $distance = $matches['distance'];
-
-    switch ($direction) {
-        case 'forward':
-            $forward += $distance;
-            $depth += $aim * $distance;
-            break;
-
-        case 'down':
-            $aim += $distance;
-            break;
-
-        case 'up':
-            $aim -= $distance;
-            break;
-    }
+    $distances[$matches['direction']] ??= [];
+    $distances[$matches['direction']][] = $matches['distance'];
 }
 
-$answer = $forward * $depth;
+foreach ($distances as $key => $values) {
+    $distances[$key] = array_sum($values);
+}
 
-die('Part 2: ' . $answer . "\n");
+$answer = $distances['forward'] * ($distances['down'] - $distances['up']);
+
+die('Part 1: ' . $answer . "\n");
